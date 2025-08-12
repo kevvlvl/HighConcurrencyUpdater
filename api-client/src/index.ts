b92c041ee05a8dc8ada55d17a12b23bd9aa1feee
kev <kev@notarealemail.com>
2025-08-11 22:48:01 -0400

    api client

--- api-client/src/index.ts
+++ api-client/src/index.ts
@@ -1,10 +1,28 @@
-import fetch from "node-fetch";
+import axios, { AxiosResponse } from "axios";
 
 console.log('Happy developing ✨')
 
-const SERVER_URL = process.env.SERVER_URL;
-const CONCURRENCY = 10;
-const ITERATIONS = 100;
+const SERVER: string = 'localhost';
+const PORT: number = 3000;
+
+axios.all([
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    axios.get(`http://${SERVER}:${PORT}/api/customer/1`),
+    ])
+    .then(axios.spread((...responses) => {
+
+        for(var index in responses) {
+            console.log("Response ", index, ": ", responses[index].status, " - Data: ", responses[index].data);
+        }
+    }));
 
 // Create a client that iterates simultaneously calling fetch/PUT. Log HTTP response code
 
